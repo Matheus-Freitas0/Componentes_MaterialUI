370c075df6fa24738c9bc85503bc4ffb8b8ca47d
Matheus-Freitas0 <moncaomatheus@gmail.com>
2025-06-19 18:16:40 -0300

    Implemented page home

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,14 +1,23 @@
-import { Button } from "@mui/material";
+import { Button, ButtonGroup } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+import "./Hero.css";
 
 export default function Hero() {
   return (
     <>
-      <div>
-        <h1>Selecione o componente desejado</h1>
-        <div className="div-buttons">
-            <Button variant="contained" color="primary">Tables</Button>
-            <Button variant="contained" color="secondary">Buttons</Button>
-        </div>
+      <div className="div-home">
+        <h1 className="titulo-home">Componentes Material-UI</h1>
+        <h3 className="subtitulo-home">Selecione o componente desejado</h3>
+        <ButtonGroup variant="contained" color="success">
+          <Button component={Link} to="/tables">
+            Tables
+          </Button>
+          <Button component={Link} to="/buttons">
+            Buttons
+          </Button>
+          <Button>teste</Button>
+        </ButtonGroup>
       </div>
     </>
   );
--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,16 +1,8 @@
 import Hero from "../components/home/hero";
-import Typography from '@mui/material/Typography'
 
 export default function PageHome() {
   return (
     <>
-      <Typography
-        variant="h3"
-        color="initial"
-        sx={{ mb: 3, fontStyle: "italic", fontWeight: "700" }}
-      >
-        Page Hero
-      </Typography>
       <Hero />
     </>
   );
